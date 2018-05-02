94ef0f2f6663ab9952d64307bf8472a9393b77fe
Debabratapal <paldebabrata82@gmail.com>
2018-05-02 19:07:42 +0530

    hashing the password feather added

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,19 +1,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt =require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token =jwt.sign(data, '123abc');
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-console.log(token);
+var hashPassword = '$2a$10$mtGqBKXGo3WwkPCrjFkuX.iv4IXp3/uWYUOgavyXJPQgx/iz6kPJO'
 
-var decoded =jwt.verify(token, '123abc');
-console.log('Decoded', decoded);
+bcrypt.compare(password, hashPassword, (err, res) => {
+  console.log(res);
+});
 
 
-
+// var data = {
+//   id: 10
+// };
+//
+// var token =jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+// var decoded =jwt.verify(token, '123abc');
+// console.log('Decoded', decoded);
+//
+//
+//
 
 
 // var message = 'I am user number 3';
