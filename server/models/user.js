27255226650d238c7f82fe69eb7c80ae57bc3d2c
Debabratapal <paldebabrata82@gmail.com>
2018-05-02 21:12:12 +0530

    logout added

--- server/models/user.js
+++ server/models/user.js
@@ -76,6 +76,20 @@ UserSchema.methods.generateAuthToken = function() {
     });
   }
 
+  UserSchema.methods.removeToken = function(token) {
+    var user = this;
+
+    return user.update({
+      $pull: {
+        tokens: {
+          token : token
+        }
+      }
+    });
+  }
+
+
+
 
   UserSchema.statics.findByToken = function(token) {
     var User = this;
